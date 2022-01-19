e0097ccc74e7e9c174fe948734717805f4b8d875
Doris Li <bubucc0629@gmail.com>
2022-01-19 17:15:23 +0800

    second commit

--- week4/verification_system.py
+++ week4/verification_system.py
@@ -4,44 +4,42 @@ from flask import redirect
 from flask import render_template
 from flask import session
 app=Flask(__name__,static_folder="public",static_url_path="/")
-app.secret_key="123456"
+app.secret_key="erythrocytosis"
 
 @app.route("/")
 def index():
     return render_template("sign_in_index.html")
 
-
-@app.route("/signin",methods=["POST"])
-def success():
+@app.route("/signin", methods = ["POST"])
+def verification():
     account_content = request.form["account_content"]
     password_content = request.form["password_content"]
-    session["account"]=account_content
-    session["password"]=password_content
+    # account_len=len(account_content)
+    # password_len=len(password_content)
     if account_content == "test" and password_content == "test":
+        session["account"] = account_content
+        session["password"] = password_content
         return redirect("/member/")
+    elif account_content == "" or password_content == "":
+        return redirect("/error/?message=請輸入帳號、密碼")
     else:
-        return redirect("/error")
-        
+        return redirect("/error/?message=帳號、或密碼輸入錯誤")
+
 @app.route("/member/")
 def member_index():
-    
     if session["password"] != None:
         return render_template("member.html")
     elif session["password"] == None:
         return render_template("sign_in_index.html")
+
 @app.route("/error/")
 def error_index():
-    account_content=session["account"]
-    password_content=session["password"]
-    account_content=len(account_content)
-    password_content=len(password_content)
-    if account_content == 0 or password_content == 0:
-        message=request.args.get("message","請輸入帳號、密碼")
-        return render_template("error.html",data=message)
-    else:
-        message=request.args.get("message","帳號、或密碼輸入錯誤")
-        return render_template("error.html",data=message)
-@app.route("/signout",methods=["GET"])
+    # if session["password"] != None:
+    #     return render_template("member.html")
+    message=request.args.get("message","")
+    return render_template("error.html",data = message)
+
+@app.route("/signout",methods = ["GET"])
 def signout():
     session["account"] = None
     session["password"] = None
